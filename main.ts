5a41a990ae9e5d198fa2966500ebde3884bef562
hinami7002 <125355289+hinami7002@users.noreply.github.com>
2023-02-14 10:39:07 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,58 @@
+let Dado = 0
+input.onGesture(Gesture.Shake, function () {
+    Dado = randint(1, 6)
+    if (6 == Dado) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    } else if (5 == Dado) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (4 == Dado) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (3 == Dado) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (2 == Dado) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (1 == Dado) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else {
+    	
+    }
+})
+basic.forever(function () {
+	
+})
